f94dd38973ac535d195179b9f6b1a895bb501a0b
Jon Banafato <jon@jonafato.com>
2022-10-26 21:31:07 -0400

    Copy additional content from old repo's web branch

--- site/__init__.py
+++ site/__init__.py
@@ -1,16 +1,24 @@
 from render_engine.blog import Blog
 from render_engine.page import Page
 from render_engine.site import Site
+from jinja2 import Environment, FileSystemLoader
 
 
 class Site(Site):
     output_path = "output"
     site_vars: dict = {
+        # TODO: add a new page type that will pull this content in on
+        # build and render it as part of the site.
+        "CODE_OF_CONDUCT_URL": "https://github.com/Python-Community-News/.github/blob/main/CODE_OF_CONDUCT.md",
         "GITHUB_URL": "https://github.com/Python-Community-News",
         "SITE_TITLE": "Python Community News",
         "SITE_URL": "https://pythoncommunitynews.com",
         "YOUTUBE_URL": "https://www.youtube.com/channel/UCA8N-T_aEhHLzwwn47K-UFw",
     }
+
+    engine = Environment(
+        loader=FileSystemLoader(["site/templates", "templates"])
+    )
 
 
 if __name__ == "__main__":

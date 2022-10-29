3629d09b4634c8eb17628f3a8fe2d0e9047cdb35
Jon Banafato <jon@jonafato.com>
2022-10-29 14:43:50 -0400

    Run pre-commit on all files

--- publish/add_episode_from_issue.py
+++ publish/add_episode_from_issue.py
@@ -1,17 +1,20 @@
-import typer
-from gh_issues import Repo, Issue
 import pathlib
+
+import typer
+from gh_issues import Issue, Repo
 from jinja2 import Environment, FileSystemLoader
 
-environment = Environment(loader=FileSystemLoader('site/templates'))
+environment = Environment(loader=FileSystemLoader("site/templates"))
 
-PCN = Repo('python-community-news', 'topics')
+PCN = Repo("python-community-news", "topics")
 
 
 def run(issue_number: int):
-    template = environment.get_template('page_template.md')
+    template = environment.get_template("page_template.md")
     current_issue = Issue.from_issue_number(repo=PCN, issue_id=issue_number)
-    return pathlib.Path(f"site/content/{current_issue.episode_date}.md").write_text(template.render(issue=current_issue))
+    return pathlib.Path(f"site/content/{current_issue.episode_date}.md").write_text(
+        template.render(issue=current_issue)
+    )
 
 
 if __name__ == "__main__":
